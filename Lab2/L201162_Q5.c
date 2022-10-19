d57d6bef715ad4b6110e05ad2aaaa607ef4fc175
farazmajid56 <farazmajid56@gmail.com>
2022-10-19 20:14:31 +0500

    Updates added

--- Lab2/L201162_Q5.c
+++ Lab2/L201162_Q5.c
@@ -6,9 +6,11 @@ int main(){
     {
         FILE *ptr = fopen("/proc/stat","r");
         char str[1024];
-        int i = 0; float U = 0; float S = 0; float I = 0; float IO = 0; float N = 0; float IR = 0; float SIR = 0; float total = 0; float useful = 0;
+        int i = 0; //float U = 0; float S = 0; float I = 0; float IO = 0; float N = 0; float IR = 0; float SIR = 0; 
+        float total = 0; 
+        float useful = 0;
 
-        while (fgets(str,1024,ptr))
+        while (fgets(str,1024,ptr) != 0)
         {
             int user,system,idle,iowait,nice,irq,softirq;
 
